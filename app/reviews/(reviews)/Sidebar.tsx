b1874bae64a05cdcbb2cce8812b7a12094b03b2e
Marcus Georgievski <marcusgeorgievski@outlook.com>
2023-07-05 20:42:41 -0400

    more sidebar

---
+++ app/reviews/(reviews)/Sidebar.tsx
@@ -0,0 +1,51 @@
+"use client";
+import { Review } from "@/lib/types";
+import Link from "next/link";
+import { useState } from "react";
+import { FiChevronDown } from "react-icons/fi";
+
+export default function Sidebar({ reviews }: { reviews: Review[] }) {
+	const [open, setOpen] = useState(true);
+
+	const handleSidebar = () => {
+		setOpen(!open);
+	};
+
+	return (
+		<nav
+			/*  */
+			/* fit" : "h-10" */
+			className={`p-2 border rounded border-slate-300 h-fit max-h-[300px]  overflow-y-scroll sticky top-6 transition-all`}
+		>
+			<div
+				onClick={handleSidebar}
+				className={`flex items-center justify-between  font-semibold  border-slate-300 cursor-pointer ${
+					!open ? "" : "border-b mb-2"
+				}`}
+			>
+				<h2>All Reviews</h2>
+				<FiChevronDown
+					className={`${open && "rotate-180"} transition-all`}
+				/>
+			</div>
+			<ul className={`flex flex-col font-mono ${!open && "hidden"} `}>
+				{reviews.map((rev) => (
+					<li
+						key={rev.slug}
+						className="px-1 py-1 text-sm rounded text-slate-600 hover:text-blue-600 hover:bg-blue-50"
+					>
+						<Link href={rev.slug} className="flex">
+							{rev.title}
+						</Link>
+					</li>
+				))}
+			</ul>
+
+			<style jsx>{`
+				* {
+					user-select: none;
+				}
+			`}</style>
+		</nav>
+	);
+}
--- app/reviews/(reviews)/layout.tsx
+++ app/reviews/(reviews)/layout.tsx
@@ -1,35 +1,15 @@
 import { getReviews } from "@/lib/reviews";
 import Link from "next/link";
+import Sidebar from "./Sidebar";
 
-export default function ReviewLayout({ children }) {
+export default async function ReviewLayout({ children }) {
+	const reviews = await getReviews();
 	return (
 		<div className="grid gap-4 grid-cols-[200px_minmax(200px,_1fr)]">
-			<aside className="p-2 border rounded border-slate-300 h-fit max-h-[300px] overflow-y-scroll sticky top-6">
-				<h2 className="mb-2 font-semibold border-b border-slate-300">
-					All Reviews
-				</h2>
-				<Sidebar />
+			<aside>
+				<Sidebar reviews={reviews} />
 			</aside>
 			<span>{children}</span>
 		</div>
-	);
-}
-
-async function Sidebar() {
-	const reviews = await getReviews();
-
-	return (
-		<ul className="flex flex-col font-mono ">
-			{reviews.map((rev) => (
-				<li
-					key={rev.slug}
-					className="px-1 py-1 text-sm rounded text-slate-600 hover:text-blue-600 hover:bg-blue-50"
-				>
-					<Link href={rev.slug} className="flex">
-						{rev.title}
-					</Link>
-				</li>
-			))}
-		</ul>
 	);
 }
